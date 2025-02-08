8644bb74ece9c1975e4a6870a97497f2b4503254
amanallahhajbrahim2003@gmail.com <amanallahhajbrahim2003@gmail.com>
2025-02-08 16:08:28 +0100

    added model and README

--- README.md
+++ README.md
@@ -1,6 +1,42 @@
 # trash-classification-twise-night-challenge
 
-###classification of trash images
-###<br>classes=[dirty,clean]
+###classification of trash images by fine tuning vgg16 on 2 classes
+###classes=[dirty,clean]
 
 ###to run this
+
+###make a python venv
+
+```
+python -m venv .venv
+cd .venv
+cd Scripts
+./activate
+```
+
+```
+pip install requirements.txt
+```
+
+###and
+
+```
+cd "mobile app"
+cd "Trash_Classification"
+npm i
+```
+
+###to start the mobile app
+
+```
+npx expo start
+```
+
+###to start the api
+
+```
+fastapi run api.py
+```
+
+###Application demo
+![plot](./demo.png)
--- mobile app/Trash_Classification/app/ImagePicker.jsx
+++ mobile app/Trash_Classification/app/ImagePicker.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { View, Button, Image, Alert, Text,TextInput } from 'react-native';
-import { launchImageLibrary } from 'react-native-image-picker';
-import axios from 'axios';
+import React, { useState } from "react";
+import { View, Button, Image, Alert, Text, TextInput } from "react-native";
+import { launchImageLibrary } from "react-native-image-picker";
+import axios from "axios";
 
 const ImagePickerComponent = () => {
   const [selectedImage, setSelectedImage] = useState(null);
@@ -11,25 +11,29 @@ const ImagePickerComponent = () => {
 
   // Function to select an image
   const pickImage = () => {
-    launchImageLibrary({ mediaType: 'photo',includeBase64:true }, (response) => {
-      if (response.didCancel) {
-        Alert.alert('Selection cancelled');
-      } else if (response.errorMessage) {
-        Alert.alert('Error:', response.errorMessage);
-      } else {
-        setSelectedImage(response.assets[0]); 
-  }});
+    launchImageLibrary(
+      { mediaType: "photo", includeBase64: true },
+      (response) => {
+        if (response.didCancel) {
+          Alert.alert("Selection cancelled");
+        } else if (response.errorMessage) {
+          Alert.alert("Error:", response.errorMessage);
+        } else {
+          setSelectedImage(response.assets[0]);
+        }
+      }
+    );
   };
 
   // Function to upload image
   const uploadImage = async () => {
     if (!selectedImage) {
-      Alert.alert('Please select an image');
+      Alert.alert("Please select an image");
       return;
     }
 
     const formData = new FormData();
-    formData.append('file', {
+    formData.append("file", {
       uri: selectedImage.uri,
       type: selectedImage.type,
       name: selectedImage.fileName,
@@ -37,7 +41,7 @@ const ImagePickerComponent = () => {
 
     // try {
     //   const response = await axios.post(
-    //     'http://127.0.0.1:8000/upload/', 
+    //     'http://127.0.0.1:8000/upload/',
     //     formData,
     //     { headers: { 'Content-Type': 'multipart/form-data' },
     //     body: JSON.stringify({
@@ -64,18 +68,24 @@ const ImagePickerComponent = () => {
       .catch((error) => {
         console.error("Error uploading image:", error);
       });
-  }
+  };
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text> Image Picker Screen</Text>
       <Button title="Select Image" onPress={pickImage} />
       {selectedImage && (
-        <Image source={{ uri: selectedImage.uri }} style={{ width: 300, height: 300 }} />
+        <Image
+          source={{ uri: selectedImage.uri }}
+          style={{ width: 300, height: 300 }}
+        />
       )}
       <Button title="Upload & Classify" onPress={uploadImage} />
-
-      <TextInput value={res.res}></TextInput>
+      <Text style={{ fontSize: 20 }}>Result:</Text>
+      <TextInput
+        style={{ fontSize: 20, textAlign: "center" }}
+        value={res.res}
+      ></TextInput>
     </View>
   );
 };
